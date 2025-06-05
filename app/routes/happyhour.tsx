be3974ecd57ba2cdc28a36b09331937557143268
Chico Sousa <ochicosousa@gmail.com>
2025-06-05 17:48:37 -0300

    nova happy

--- app/routes/happyhour.tsx
+++ app/routes/happyhour.tsx
@@ -1,25 +1,21 @@
 import { useEffect } from "react";
 
 export default function HappyHour() {
-	useEffect(() => {
-		location.href =
-			// "https://parkchoppsobral.goomer.app/menu?hash=2eNZw#1498899";
-			// location.href = "https://parksobral.saipos.com";
-			location.href =
-				"https://drive.google.com/file/d/1dtk8Lr8-jIr_AVCnzbFvm-DbvyeMYsP1/view?usp=share_link";
-	}, []);
+  useEffect(() => {
+    location.href =
+      "https://drive.google.com/file/d/1lBA2LJZR71kLi0F1D-FFqnlzaUldX3fz/view?usp=drive_link";
+  }, []);
 
-	return (
-		<div className="grid w-screen text-center h-screen place-content-center p-8 text-sobral bg-familia">
-			<div className="mb-8">
-				Estamos te levando <br /> para o nosso{" "}
-				<strong>CARDÁPIO...</strong>
-			</div>
-			<img
-				src="/loader.png"
-				alt="Estamos te levando para o nosso cardápio..."
-				className="animate-spin mx-auto w-12"
-			/>
-		</div>
-	);
+  return (
+    <div className="grid w-screen text-center h-screen place-content-center p-8 text-sobral bg-familia">
+      <div className="mb-8">
+        Estamos te levando <br /> para o nosso <strong>CARDÁPIO...</strong>
+      </div>
+      <img
+        src="/loader.png"
+        alt="Estamos te levando para o nosso cardápio..."
+        className="animate-spin mx-auto w-12"
+      />
+    </div>
+  );
 }
