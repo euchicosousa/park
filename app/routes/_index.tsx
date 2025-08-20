67331eb34806cbf971f520548acff903735a0241
Chico Sousa <ochicosousa@gmail.com>
2025-08-20 10:28:31 -0300

    localizacao

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,42 +3,42 @@ import { MetaFunction } from "@vercel/remix";
 import { BeerIcon, BikeIcon, BookIcon, MartiniIcon } from "lucide-react";
 
 export const meta: MetaFunction = () => {
-	return [
-		{ title: "PARK - Restaurante em Sobral" },
-		{
-			name: "description",
-			content:
-				"O PARK é um restaurante familiar em Sobral, oferecendo pratos saborosos, happy hour animado e música ao vivo em um ambiente acolhedor.",
-		},
-	];
+  return [
+    { title: "PARK - Restaurante em Sobral" },
+    {
+      name: "description",
+      content:
+        "O PARK é um restaurante familiar em Sobral, oferecendo pratos saborosos, happy hour animado e música ao vivo em um ambiente acolhedor.",
+    },
+  ];
 };
 
 export default function Index() {
-	return (
-		<div className="w-full h-screen  flex flex-col">
-			<div
-				className="bg-sabor bg-cover bg-center w-full grow shrink-0 "
-				style={{ backgroundImage: "url(./casa.jpg)" }}
-			></div>
-			<div
-				className="bg-familia grow-0 shrink-0 px-4 bg-cover bg-center"
-				style={{ backgroundImage: "url(./bg-park.png)" }}
-			>
-				<div className="bg-familia border-4 border-sobral w-32 p-4 rounded-full mx-auto -mt-16">
-					<img src="./logo-park.png" alt="" />
-				</div>
-				{/* link */}
-				<div className="flex flex-col mt-8 gap-2 max-w-sm mx-auto">
-					<Link
-						to="/delivery"
-						target="_blank"
-						rel="noreferrer"
-						className="link"
-					>
-						<BikeIcon className="size-8" />
-						<span>Pedir Delivery</span>
-					</Link>
-					{/* <Link
+  return (
+    <div className="w-full h-screen  flex flex-col">
+      <div
+        className="bg-sabor bg-cover bg-center w-full grow shrink-0 "
+        style={{ backgroundImage: "url(./casa.jpg)" }}
+      ></div>
+      <div
+        className="bg-familia grow-0 shrink-0 px-4 bg-cover bg-center"
+        style={{ backgroundImage: "url(./bg-park.png)" }}
+      >
+        <div className="bg-familia border-4 border-sobral w-32 p-4 rounded-full mx-auto -mt-16">
+          <img src="./logo-park.png" alt="" />
+        </div>
+        {/* link */}
+        <div className="flex flex-col mt-8 gap-2 max-w-sm mx-auto">
+          <Link
+            to="/delivery"
+            target="_blank"
+            rel="noreferrer"
+            className="link"
+          >
+            <BikeIcon className="size-8" />
+            <span>Pedir Delivery</span>
+          </Link>
+          {/* <Link
 						to="/cardapio"
 						target="_blank"
 						rel="noreferrer"
@@ -47,62 +47,61 @@ export default function Index() {
 						<BookIcon className="size-8" />
 						<span>Cardápio Completo</span>
 					</Link> */}
-					<Link
-						to="/happyhour"
-						target="_blank"
-						rel="noreferrer"
-						className="link"
-					>
-						<BeerIcon className="size-8" />
-						<span>Happy Hour</span>
-					</Link>
+          <Link
+            to="/happyhour"
+            target="_blank"
+            rel="noreferrer"
+            className="link"
+          >
+            <BeerIcon className="size-8" />
+            <span>Happy Hour</span>
+          </Link>
 
-					<Link
-						to="/eventos"
-						// to="https://wa.me/558821440310?text=Quero realizar um evento no PARK."
-						target="_blank"
-						rel="noreferrer"
-						className="link"
-					>
-						{/* <CakeIcon className="size-8" /> */}
-						<MartiniIcon className="size-8" />
-						Realize seu evento conosco
-					</Link>
-				</div>
-				{/* Social */}
-				<div className="flex gap-4 my-8 text-sm text-sobral justify-center">
-					<a
-						href="https://instagram.com/parksobral"
-						className="flex gap-2 font-bold items-center"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<span>Instagram</span>
-					</a>
-					<a
-						href="https://maps.app.goo.gl/PAUtxtJBSXKBLFp39"
-						className="flex gap-2 font-bold items-center"
-						target="_blank"
-						rel="noreferrer"
-					>
-						{/* <MapPinIcon className="size-6" /> */}
-						<span>Como chegar</span>
-					</a>
-					<a
-						href="https://wa.me/558821440310"
-						className="flex gap-2 font-bold items-center"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<span>WhatsApp</span>
-					</a>
-				</div>
-				{/* Copyrights */}
-				<div className="text-xs text-sobral opacity-50 pb-8  text-center">
-					© {new Date().getFullYear()} PARK - Sua aventura culinária
-					em Sobral
-				</div>
-			</div>
-		</div>
-	);
+          <Link
+            to="/eventos"
+            // to="https://wa.me/558821440310?text=Quero realizar um evento no PARK."
+            target="_blank"
+            rel="noreferrer"
+            className="link"
+          >
+            {/* <CakeIcon className="size-8" /> */}
+            <MartiniIcon className="size-8" />
+            Realize seu evento conosco
+          </Link>
+        </div>
+        {/* Social */}
+        <div className="flex gap-4 my-8 text-sm text-sobral justify-center">
+          <a
+            href="https://instagram.com/parksobral"
+            className="flex gap-2 font-bold items-center"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>Instagram</span>
+          </a>
+          <a
+            href="/localizacao"
+            className="flex gap-2 font-bold items-center"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {/* <MapPinIcon className="size-6" /> */}
+            <span>Como chegar</span>
+          </a>
+          <a
+            href="https://wa.me/558821440310"
+            className="flex gap-2 font-bold items-center"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>WhatsApp</span>
+          </a>
+        </div>
+        {/* Copyrights */}
+        <div className="text-xs text-sobral opacity-50 pb-8  text-center">
+          © {new Date().getFullYear()} PARK - Sua aventura culinária em Sobral
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/routes/localizacao.tsx
+++ app/routes/localizacao.tsx
@@ -1,20 +1,19 @@
 import { useEffect } from "react";
 
-export default function Eventos() {
+export default function Delivery() {
   useEffect(() => {
-    location.href = `https://wa.me/558821440310?text=${encodeURI(
-      "Quero realizar um evento no PARK."
-    )}`;
+    location.href = "https://maps.app.goo.gl/PAUtxtJBSXKBLFp39";
   }, []);
 
   return (
     <div className="grid w-screen text-center h-screen place-content-center p-8 text-sobral bg-familia">
       <div className="mb-8">
-        Estamos te levando <br /> para o nosso WHATSAPP...
+        Estamos te levando <br /> para o mapa com a nossa{" "}
+        <strong>LOCALIZAÇÃO...</strong>
       </div>
       <img
         src="/loader.png"
-        alt="Estamos te levando para o nosso WHATSAPP..."
+        alt="Estamos te levando para o mapa com a nossa Localização Delivery..."
         className="animate-spin mx-auto w-12"
       />
     </div>
