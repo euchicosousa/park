75caea728a3b6380916d3f4f198f68d48a865d5b
Chico Sousa <ochicosousa@gmail.com>
2026-03-01 21:41:43 -0300

    og image

--- app/root.tsx
+++ app/root.tsx
@@ -10,23 +10,25 @@ import "./tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1"
-				/>
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				{children}
-				<ScrollRestoration />
-				<Scripts />
-			</body>
-		</html>
-	);
+    <html lang="pt-br">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <meta
+          property="og:image"
+          content="./og-park.png"
+        />
+
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
 
 export default function App() {
